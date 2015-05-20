01199f20414024908700bedbdea1e60b51e69d3d
Alex Min <alex.m.4155@gmail.com>
2015-05-20 15:35:12 -0400

    Created simple hello world home page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+# This pages controller was created in order to show the roots from routes
+
+class PagesController < ApplicationController
+	def home
+	end
+end
+
+# If the pages controller doesnt know what to do, then the application controller probably will
